1f4789107bc269de0e6e087d8e17eb6d151c8b11
PiotrO9 <piotr.ostrowski02@wp.pl>
2024-04-28 07:27:34 +0200

    Add proper and working prettier and eslint ocnfiguration

---
+++ .eslintrc.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+    env: {
+        browser: true,
+        node: true,
+    },
+    extends: ['plugin:vue/recommended', 'eslint:recommended'],
+    rules: {
+        'vue/no-unused-vars': 'error',
+    },
+}
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
-  css: ["./assets/styles/global.scss"],
-});
+    devtools: { enabled: true },
+    css: ['./assets/styles/global.scss'],
+    modules: ['@nuxtjs/eslint-module'],
+    typescript: {
+        typeCheck: true,
+    },
+})
