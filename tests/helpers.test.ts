62ef4dfecb572ebaf5c5d0205f94d2b5cde2dbef
PiotrO9 <piotr.ostrowski02@wp.pl>
2024-04-30 19:14:57 +0200

    Add configuration and first examples of unit tests

---
+++ tests/helpers.test.ts
@@ -0,0 +1,10 @@
+import capitalizeFirstLetter from '../assets/helpers/string-manipulation/capitalizeFirstLetter'
+
+describe('helpers function tests', () => {
+	test('Capitalize first letter test', () => {
+		const word = 'test'
+		const result = capitalizeFirstLetter(word)
+
+		expect(result).toBe('Test')
+	})
+})
---
+++ tests/imports.test.ts
@@ -0,0 +1,6 @@
+describe('import vue components', () => {
+	test('normal imports as expected', async () => {
+		const cmp = await import('../components/Base/BaseButton.vue')
+		expect(cmp).toBeDefined()
+	})
+})
